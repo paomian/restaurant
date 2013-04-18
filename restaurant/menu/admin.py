461059d4b2261dbc42278fa81b7c31726afc73ea
“paomian” <tanglnan1994@gmail.com>
2013-04-18 10:57:42 +0800

    4.1

--- restaurant/menu/admin.py
+++ restaurant/menu/admin.py
@@ -4,5 +4,6 @@ from restaurant.menu.models import *
 admin.site.register(Customer)
 admin.site.register(Dish)
 admin.site.register(Desk)
-admin.site.register(Room)
-admin.site.register(Relationships)
+#admin.site.register(Room)
+admin.site.register(Dishship)
+admin.site.register(Menu)
--- restaurant/menu/models.py
+++ restaurant/menu/models.py
@@ -20,8 +20,8 @@ class Dish(models.Model):              #菜类
 			)
 	name = models.CharField("菜名",max_length=30)
 	price = models.DecimalField("价格",max_digits=5,decimal_places=2)
-	photo = models.ImageField("菜品图片",upload_to='dish_photo')
-	description = models.TextField("菜品介绍",)
+	photo = models.ImageField("菜品图片",upload_to='dish_photo',null=True,blank=True)
+	description = models.TextField("菜品介绍",null=True,blank=True)
 	category = models.DecimalField("分类",max_digits=2,decimal_places=0,choices=CATEGORY_CHOICE)
 	be_chice = models.DecimalField("被点数",max_digits=10,decimal_places=0)
 #	desk = models.ForeignKey()
@@ -40,34 +40,47 @@ class Desk(models.Model):             #餐桌
 	behaviour = models.DecimalField("状态",max_digits=1,decimal_places=0,choices=BEHAVIOUR_CHOICE)
 	max_person = models.DecimalField("承受最大人数",max_digits=2,decimal_places=0)
 	chair = models.DecimalField("椅子数",max_digits=2,decimal_places=0)
-	description = models.TextField("备注",null=True)
+	description = models.TextField("备注",null=True,blank=True)
+	dish = models.ManyToManyField(Dish,through='Dishship')
 #	dish = models.ForeignKey(Desk_Dish)
 #	room_id = models.DecimalField(max_digits=2,decimal_places=0)
 	def __unicode__(self):
 		return unicode(self.id)
-class Room(models.Model):             #房间
-	desk_num = models.DecimalField("桌子编号",max_digits=2,decimal_places=0)
-	desk = models.ForeignKey(Desk)
-	description = models.TextField("备注",null=True)
+#class Room(models.Model):             #房间
+#	desk_num = models.DecimalField("桌子编号",max_digits=2,decimal_places=0)
+#	desk = models.ForeignKey(Desk)
+#	description = models.TextField("备注",null=True)
 #	customer_id = models.DecimalField(max_digits=2,decimal_places=0)
+#	def __unicode__(self):
+#		return unicode(self.id) 
+class Customer(models.Model):
+	customer_num = models.DecimalField(max_digits=3,decimal_places=0)
+	time = models.DateTimeField("就餐时间",)
 	def __unicode__(self):
 		return unicode(self.id) 
-class Customer(models.Model):
-	person_num = models.DecimalField("顾客人数",max_digits=2,decimal_places=0)
-	date = models.DateField("就餐时间",)
+class Menu(models.Model):
+	date = models.DateTimeField("时间",)
 	consumption= models.DecimalField("总消费",max_digits=5,decimal_places=0)
-	room = models.ManyToManyField(Room)
 	chair = models.DecimalField("椅子数",max_digits=2,decimal_places=0)
 	tableware = models.DecimalField("餐具数",max_digits=2,decimal_places=0)
-	description = models.TextField("备注",null=True)
+	description = models.TextField("备注",null=True,blank=True)
+	customer = models.ForeignKey(Customer)
 	def __unicode__(self):
 		return unicode(self.id)
-class Relationships(models.Model):
-	desk = models.ForeignKey(Desk)
+#class Relationships(models.Model):
+#	desk = models.ForeignKey(Desk)
+#	dish = models.ForeignKey(Dish)
+#	customer = models.ForeignKey(Customer)
+#	class Meta:
+#		unique_together = ('desk','dish','customer')
+#	dish_num = models.DecimalField("菜品数量",max_digits=2,decimal_places=0)
+#	def __unicode__(self):
+#		return unicode(self.customer)
+class Dishship(models.Model):
 	dish = models.ForeignKey(Dish)
+	desk = models.ForeignKey(Desk)
 	customer = models.ForeignKey(Customer)
-	class Meta:
-		unique_together = ('desk','dish','customer')
-	dish_num = models.DecimalField("菜品数量",max_digits=2,decimal_places=0)
+	time = models.DateTimeField()
+	dish_num = models.DecimalField("菜品数",max_digits=2,decimal_places=0)
 	def __unicode__(self):
-		return unicode(self.customer)
+		return unicode(self.desk)
